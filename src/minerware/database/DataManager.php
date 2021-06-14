fc3ac258c8b3e8c99c07edffcb2b80a44aac6c6d
ヨルダン <46408545+JustJ0rd4n@users.noreply.github.com>
2021-06-14 10:11:42 -0400

    Added player data storage manager and initialize multi storage type support.

--- src/minerware/database/DataManager.php
+++ src/minerware/database/DataManager.php
@@ -19,6 +19,7 @@ declare(strict_types=1);
 namespace minerware\database;
 
 use minerware\Minerware;
+use pocketmine\player\Player;
 use pocketmine\utils\Config;
 use pocketmine\utils\SingletonTrait;
 
@@ -31,11 +32,36 @@ final class DataManager {
     
     private Config $config;
     
+    private int $arenaStorageType = 1;
+    
+    private int $playerStorageType = 1;
+    
     public function __construct() {
         $this->plugin = Minerware::getInstance();
         $this->pluginPath = $this->plugin->getDataFolder();
         $this->config = $this->plugin->getConfig();
         
+        $formats = Config::$formats;
+        $formats["nbt"] = 6;
+        $formats["namedtag"] = $formats["nbt"];
+        
+        $this->arenaStorageType = $formats[$this->config->getNested("storage-format.arena-data")];
+        $this->playerStorageType = $formats[$this->config->getNested("storage-format.player-data")];
+        
         @mkdir($this->pluginPath . "database" . DIRECTORY_SEPARATOR);
+        @mkdir($this->pluginPath . "database" . DIRECTORY_SEPARATOR . "players" . DIRECTORY_SEPARATOR);
+    }
+    
+    /**
+     * TODO:: Add multi storage type support.
+     */
+    public function getPlayerData(string|Player $player): ?DataHolder {
+        $filePath = "players" . DIRECTORY_SEPARATOR . (($player instanceof Player) ? $player->getName() : $player) . ".json";
+        $path = $this->pluginPath . "database" . DIRECTORY_SEPARATOR . $path;
+        if (file_exists($path)) {
+            return new DataHolder((new Config($path, $this->playerStorageType))->getAll());
+        }
+        
+        return null;
     }
 }
