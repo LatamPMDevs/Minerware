34c73dfc1a067a053bb397ae4a4a032d58bf87f9
ヨルダン <46408545+JustJ0rd4n@users.noreply.github.com>
2021-12-11 13:32:38 -0500

    Update .php-cs-fixer.php

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 $finder = PhpCsFixer\Finder::create()
 	->in(__DIR__ . "/src");
 
-	return (new PhpCsFixer\Config)
+return (new PhpCsFixer\Config)
 	->setRiskyAllowed(true)
 	->setRules([
 		"align_multiline_comment" => [
