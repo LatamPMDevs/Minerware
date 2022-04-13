c598912b4f868cd7335962977e11f38c1473b3f3
ヨルダン <46408545+JustJ0rd4n@users.noreply.github.com>
2022-04-13 11:25:10 -0400

    revert https://github.com/LatamPMDevs/Minerware/commit/6446f74511b6952f1179ac48d26c8ef8abe8fe6b

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -63,8 +63,11 @@ return (new PhpCsFixer\Config)
 		"single_import_per_statement" => true,
 		"strict_param" => true,
 		"return_assignment" => true,
+		"return_type_declaration" => [
+			"space_before" => "one"
+		],
 		"function_declaration" => [
-			"closure_function_spacing" => "none"
+			"closure_function_spacing" => "one"
 		]
 	])
 	->setFinder($finder)
